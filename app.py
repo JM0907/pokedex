9df4f1c4b26e4b12d64804638b43785a51d0f7d9
JM0907 <135572091+JM0907@users.noreply.github.com>
2023-12-04 19:41:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<p>Hello, World!</p>"
+    return render_template('pokemon.html')
 
-if __name__ == __main__:
-    ap.run(debug=True)
+@app.route("/a")
+def home():
+    return render_template('detalle.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
