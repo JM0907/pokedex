581c442e777be8d99b9c061ed784367d3a2cd950
JM0907 <135572091+JM0907@users.noreply.github.com>
2023-12-06 19:50:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
-
+import requests
 app = Flask(__name__)
+
+def get_pokemon_data(pokemon):
+    url = f'https://pokeapi.co/api/v2/pokemon/{pokemon}'
+    r = requests.get(url).json()
+    return r        
 
 @app.route("/")
 def home():
@@ -10,9 +15,7 @@ def home():
 def detalle():
     return render_template('detalle.html')
 
-
 if __name__ == '__main__':
     app.run(debug=True)
-
 
 
