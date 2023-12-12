2f16f4794642e7581c47c538695f4d3e39436aa2
JM0907 <135572091+JM0907@users.noreply.github.com>
2023-12-11 19:57:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template
 import requests
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped,mapped_column
+
+
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pokedex.sqlite"
+
+db = SQLAlchemy(app)
+
+
+class Pokedex (db.Model):
+    id: Mapped[int] = mapped_column(db.Integer, primary_key=True,autoincrement=True)
+    name: Mapped[str] = mapped_column(db.String, nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 def get_pokemon_data(pokemon):
     url = f'https://pokeapi.co/api/v2/pokemon/{pokemon}'
@@ -15,7 +33,25 @@ def home():
 def detalle():
     return render_template('detalle.html')
 
+
+
+#PRUEBA
+@app.route("/insert")
+def insert():
+    new_pokemon = 'charmander'
+    if new_pokemon:
+        obj=Pokedex(name=new_pokemon)
+        db.session.add(obj)
+        db.session.commit()
+    return 'pokemon agregado'
+
+
+@app.route("/select")
+def select():
+    lista_pokemon=Pokedex.query.all()
+    return lista_pokemon
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
 
