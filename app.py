a66dcf5bf5c8e1940053b363498719e81ef04b5e
JM0907 <135572091+JM0907@users.noreply.github.com>
2023-12-13 13:13:16 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
-import requests
+from flask import Flask, render_template,jsonify
+import requests 
 
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped,mapped_column
+from sqlalchemy.orm import Mapped, mapped_column
 
 
 app = Flask(__name__)
@@ -13,17 +13,19 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pokedex.sqlite"
 db = SQLAlchemy(app)
 
 
-class Pokedex (db.Model):
+class Pokemon(db.Model):
     id: Mapped[int] = mapped_column(db.Integer, primary_key=True,autoincrement=True)
-    name: Mapped[str] = mapped_column(db.String, nullable=False)
+    name: Mapped[str] = mapped_column(db.String, unique=True, nullable=False)
+
 
 with app.app_context():
     db.create_all()
 
+
 def get_pokemon_data(pokemon):
     url = f'https://pokeapi.co/api/v2/pokemon/{pokemon}'
     r = requests.get(url).json()
-    return r        
+    return r
 
 @app.route("/")
 def home():
@@ -34,24 +36,31 @@ def detalle():
     return render_template('detalle.html')
 
 
-
-#PRUEBA
 @app.route("/insert")
 def insert():
-    new_pokemon = 'charmander'
+    new_pokemon= 'charmander'
     if new_pokemon:
-        obj=Pokedex(name=new_pokemon)
-        db.session.add(obj)
-        db.session.commit()
-    return 'pokemon agregado'
-
+            obj = Pokemon(name=new_pokemon)
+            db.session.add(obj)
+            db.session.commit()
+    return 'Pokemon Agregado'
 
 @app.route("/select")
 def select():
-    lista_pokemon=Pokedex.query.all()
-    return lista_pokemon
+    lista_pokemon= Pokemon.query.all()
+    for p in lista_pokemon:
+        print(p.name)
+
+    return 'alo'
 
 
-if __name__ == '__main__':
+@app.route("/select/<name>")
+def selectbyname(name):
+    poke= Pokemon.query.filter_by(name=name).first()
+    return str(poke.id)
+
+
+
+
+if __name__=='__main__':
     app.run(debug=True)
-
