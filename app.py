e9aa07ded317cc971a8d5f0b62a5c327c895a5ec
JM0907 <135572091+JM0907@users.noreply.github.com>
2023-12-19 23:05:15 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, jsonify, request
-import requests 
+from flask import Flask, render_template, request
+import requests
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Column, Integer, String, Float
 
 app = Flask(__name__)
 
@@ -10,12 +10,12 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pokedex.sqlite"
 db = SQLAlchemy(app)
 
 class Pokemon(db.Model):
-    id:Mapped[int] = mapped_column(db.Integer, primary_key=True,autoincrement=True)
-    name:Mapped[str]= mapped_column(db.String,nullable=False)
-    height:Mapped[float] = mapped_column(db.Float,nullable=False)
-    weight: Mapped[float] = mapped_column(db.Float, nullable=False)
-    order: Mapped[int] = mapped_column(db.Integer,nullable=False)
-    type :Mapped[str]= mapped_column(db.String,nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    height = Column(Float, nullable=False)
+    weight = Column(Float, nullable=False)
+    order = Column(Integer, nullable=False)
+    type = Column(String, nullable=False)
 
 with app.app_context():
     db.create_all()
@@ -28,51 +28,74 @@ def get_pokemon_data(pokemon):
 @app.route("/", methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        pokemon_name = request.form['pokemon_name']
-        return search_pokemon(pokemon_name)
-
+        pokemon_name = request.form.get('pokemon_name')
+        if pokemon_name:
+            return search_pokemon(pokemon_name)
     return render_template('pokemon.html', pokemon=None)
 
 @app.route("/pokemon/<name>")
 def search_pokemon(name):
     data = get_pokemon_data(name.lower())
-    if 'sprites' not in data or 'official-artwork' not in data['sprites']['other']:
+    if 'sprites' not in data or 'other' not in data['sprites']:
         return render_template('pokemon.html', pokemon=None)
+
+    other_sprites = data['sprites']['other']
     
+    # Verifica si 'official-artwork' y 'dream-artwork' están presentes
+    official_artwork = other_sprites.get('official-artwork', {}).get('front_default')
+    dream_artwork = other_sprites.get('dream-artwork', {}).get('front_default')
+
+    if not official_artwork:
+        return render_template('pokemon.html', pokemon=None)
+
     pokemon = {
-        'photo': data['sprites']['other']['official-artwork']['front_default'],
         'name': data['name'].upper(),
         'height': data['height'],
         'weight': data['weight'],
-        'order': data['order'], 
-        'type': '',
+        'order': data['order'],
+        'type': 'Estudiante',
+        'hp': data.get('stats')[0].get('base_stat'),
+        'attack': data.get('stats')[1].get('base_stat'),
+        'defence': data.get('stats')[2].get('base_stat'),
+        'speed': data.get('stats')[5].get('base_stat'),
+        'photo': official_artwork,
+        'photo1': dream_artwork
     }
+
     return render_template('pokemon.html', pokemon=pokemon)
 
-@app.route("/detalle")
-def detalle():
-    return render_template('detalle.html')
 
-@app.route("/insert_pokemon/<pokemon>")
-def insert(pokemon):
-    new_pokemon = pokemon
-    if new_pokemon:
-         obj = Pokemon(pokemon)
-         db.session.add(obj)
-         db.session.commit()
-    return 'Pokemon Agregado'
+@app.route("/detalle/<hp>/<attack>/<defence>/<speed>/")
+def detalle(hp, attack, defence, speed):
+    return render_template('detalle.html', hp=hp, attack=attack, defence=defence, speed=speed)
+
+@app.route("/insert_pokemon/<pokemon_name>")
+def insert(pokemon_name):
+    data = get_pokemon_data(pokemon_name)
+    if data:
+        new_pokemon = Pokemon(
+            name=data['name'],
+            height=data['height'],
+            weight=data['weight'],
+            order=data['order'],
+            type='Estudiante'
+        )
+        db.session.add(new_pokemon)
+        db.session.commit()
+        return 'Pokemon Agregado'
+    return 'No se pudo obtener información del Pokemon'
 
 @app.route("/select")
 def select():
-    lista_pokemon= Pokemon.query.all()
+    lista_pokemon = Pokemon.query.all()
     for p in lista_pokemon:
         print(p.name)
     return 'alo'
 
 @app.route("/selectbyname/<name>")
 def selectbyname(name):
-    poke= Pokemon.query.filter_by(name=name).first()
-    return str(poke.id),str(poke.name)
+    poke = Pokemon.query.filter_by(name=name).first()
+    return str(poke.id), str(poke.name)
 
 @app.route("/selectbyid/<id>")
 def selectbyid(id):
@@ -86,5 +109,5 @@ def deletetbyid(id):
     db.session.commit()
     return 'Pokemon Eliminado'
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
